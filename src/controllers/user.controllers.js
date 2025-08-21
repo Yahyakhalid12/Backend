401714880a6ba01c7b5249dae93b00cfc8f26f83
Yahya Khalid <yahyakhalid248@gmail.com>
2025-08-21 23:34:15 +0500

    debugging done

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -38,7 +38,8 @@ const registerUser = asyncHandler(async (req, res) => {
     
     const avatarLocalPath = req.files?.avatar?.[0]?.path;
     const coverImageLocalPath = req.files?.coverImage?.[0]?.path;
-    
+
+    console.log(req.files);
     console.log("avatarLocalPath:", avatarLocalPath);
     console.log("coverImageLocalPath:", coverImageLocalPath);
 
